73f20f7148e2e379c51514ec4829bf85496eaa86
Neeraj Kumbhkar <neerajkumbhkar58@gmail.com>
2023-06-03 09:33:09 +0530

    edit

--- src/main/scala/ClaimStaires.scala
+++ src/main/scala/ClaimStaires.scala
@@ -1,6 +1,5 @@
 
 /*Climb Stairs
-Easy  Prev   Next
 1. You are given a number n, representing the number of stairs in a staircase.
 2. You are on the 0th step and are required to climb to the top.
 3. In one move, you are allowed to climb 1, 2 or 3 stairs.
